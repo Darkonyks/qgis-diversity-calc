a6df7acde80be4299d4cefe6ce7f997f33126b59
Darko Nedic <darko.nedic83@gmail.com>
2021-02-08 17:19:52 -0500

    Plugin finished

--- help/source/conf.py
+++ help/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DiversityCalc'
-copyright = u'2013, GeoDigit'
+copyright = u'2013, Miller Mountain LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -179,7 +179,7 @@ htmlhelp_basename = 'TemplateClassdoc'
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'DiversityCalc.tex', u'DiversityCalc Documentation',
-   u'GeoDigit', 'manual'),
+   u'Miller Mountain LLC', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -212,5 +212,5 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'TemplateClass', u'DiversityCalc Documentation',
-     [u'GeoDigit'], 1)
+     [u'Miller Mountain LLC'], 1)
 ]
